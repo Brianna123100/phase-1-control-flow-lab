b388fe1d7f206d542fb34b6db39c4967864e6aa3
Brianna Ponder <lemleybrianna123100@gmail.com>
2023-07-02 15:44:00 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+  if (distance <= 400) {
+    result = 'This one is on me!'
+  } else if (distance > 400 && distance < 2000) {
+    result = "That will be twenty bucks."
+  } else if (distance > 2000 && distance < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500) {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response
 }
